995b4f3f9fc1e6274b28ffa9bbbc34f313e84387
Chandramogali Gomasi <92980776+chandramogali@users.noreply.github.com>
2023-05-29 22:45:28 +0530

    Update Inputs.jsx

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -12,22 +12,6 @@ function Inputs({setQuery, units, setUnits}) {
     if(city !=='') setQuery({q:city})
   }
 
-  // const handleLocationClick =()=> {
-
-  //   if(navigator.geolocation ){
-  //     navigator.geolocation.getCurrentPosition((position)=>{
-  //       let lat=position.coords.latitude;
-  //       let lon=position.coords.longitude;
-  //       setQuery({
-  //         lat,lon,
-  //       });
-  //     })
-  //   }
-  // }
-
-
-   
-
   return (
    <div className="flex flex-row justify-center my-6">
      <div className="flex flex-row w-3/4 items-center justify-center space-x-4">
@@ -40,21 +24,9 @@ function Inputs({setQuery, units, setUnits}) {
     size={25} className="text-white cursor-pointer transition ease-out hover:scale-150"
     onClick={handleSearchClick}
      />
-    {/* <UilLocationPoint size={25} className="text-white cursor-pointer transition ease-out hover:scale-150 "
-      onClick={handleLocationClick}
-    />
-         */}
+    
      </div>
 
-     {/* <div className="flex flex-row w-1/4 items-center justify-center ">
-        <button name="metric" className="text-xl text-white font-light transition:ease-out hover:scale-125" 
-        onClick={handleUnitChange}
-        > ℃</button>
-        <p className="text-xl text-white mx-2 ">|</p>
-        <button name="imperial" className="text-xl text-white font-light transition:ease-out hover:scale-125"
-         onClick={handleUnitChange}
-         >℉</button>
-     </div> */}
    </div>
   )
 }
